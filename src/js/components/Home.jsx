8d58851d043ee5152213686490374772b3b33811
carlossb21 <carlosandresb21@gmail.com>
2025-05-02 23:28:13 +0000

    cambios todolist2.0

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
@@ -10,6 +10,7 @@ const Home = () => {
 
 
 	let maximoTareas = 5;
+	let nombreUsuarioAPI = 'pedrogomez'
 
 	const agregar = (e) => {
 		//verifica si la tecla es enter
@@ -20,7 +21,35 @@ const Home = () => {
 			}
 			//limite de tareas
 			if (todos.length < maximoTareas) {
-				todos.push(texto);
+				
+				fetch('https://playground.4geeks.com/todo/todos/' + nombreUsuarioAPI, {
+					method: "POST",
+					body: JSON.stringify({
+						label: texto,
+						is_done: false
+					  }),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+				.then(resp => {
+					//console.log(resp.ok); // Será true si la respuesta es exitosa
+					//console.log(resp.status); // El código de estado 201, 300, 400, etc.
+					return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+				})
+				.then(data => {
+					// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+					//console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+					
+					console.log(data);
+					setTodos([...todos,data]);//spread
+		
+				})
+				.catch(error => {
+					// Manejo de errores
+					console.log(error);
+				});
+
 			} else {
 				alert('solo puedes agregar maximo ' + maximoTareas + ' elementos');
 			}
@@ -31,18 +60,27 @@ const Home = () => {
 	}
 	const obtenerTareas = () => {
 
-		fetch('https://playground.4geeks.com/todo/users/prueba123', {
+		fetch('https://playground.4geeks.com/todo/users/' + nombreUsuarioAPI, {
 			method: "GET"
 		})
 		.then(resp => {
-			console.log(resp.ok); // Será true si la respuesta es exitosa
-			console.log(resp.status); // El código de estado 201, 300, 400, etc.
+			//console.log(resp.ok); // Será true si la respuesta es exitosa
+			//console.log(resp.status); // El código de estado 201, 300, 400, etc.
 			return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
 		})
 		.then(data => {
 			// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-			console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-			setTodos(data)
+			//console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+			
+			let todosapi = [];
+			data.todos.map((item)=>{
+				console.log(item);
+				todosapi.push(item);
+			});
+			
+			setTodos(todosapi);
+
+
 		})
 		.catch(error => {
 			// Manejo de errores
@@ -55,10 +93,38 @@ const Home = () => {
 		setTexto(e.target.value);
 	}
 
-	const eliminarTarea = (paramIndex) => {
-		let elementos = todos.filter((item, index) => index !== paramIndex);
-		setTodos(elementos);
+	const eliminarTarea = (paramId) => {
+
+		console.log(paramId);
+
+		fetch('https://playground.4geeks.com/todo/todos/' + paramId , {
+			method: "DELETE"
+		})
+		.then(resp => {
+			//console.log(resp.ok); // Será true si la respuesta es exitosa
+			console.log(resp);
+			//console.log(resp.status); // El código de estado 201, 300, 400, etc.
+			return resp;
+		})
+		.then(data => {
+			
+			console.log(data);
+			let elementos = todos.filter((item) => item.id !== paramId);
+			setTodos(elementos);
+
+		})
+		.catch(error => {
+			// Manejo de errores
+			console.log(error);
+		});
+
+		
 	}
+
+
+	useEffect(()=>{
+		obtenerTareas();
+	},[]);
 
 	return (
 		<div className="row justify-content-center">
@@ -75,9 +141,9 @@ const Home = () => {
 					</li>
 					{
 						todos.map((item, index) => (
-							<li className="list-group-item tarea" key={index}>
-								{item}
-								<span className="float-end btnEliminar d-none" onClick={() => eliminarTarea(index)}>X</span>
+							<li className="list-group-item tarea" key={item.id}>
+								{item.label}
+								<span className="float-end btnEliminar d-none" onClick={() => eliminarTarea(item.id)}>X</span>
 							</li>
 						))
 					}
